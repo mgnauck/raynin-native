5487fbd3287bfec54504af5896846f0e6526e3df
Markus Gnauck <17846097+mgnauck@users.noreply.github.com>
2025-03-04 21:06:48 +0100

    scene fix

--- src/scene.h
+++ src/scene.h
@@ -25,6 +25,6 @@ typedef struct scene {
 } scene;
 
 void scene_init(scene *scene, uint32_t mesh_cnt, uint32_t inst_cnt, uint32_t mat_cnt);
-void scene_release();
+void scene_release(scene *scene);
 
 #endif
